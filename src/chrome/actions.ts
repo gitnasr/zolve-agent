927bebf44155cadfba1ab03439139a6d7409cf7e
Mahmoud Nasr <239.nasr@gmail.com>
2025-02-25 13:16:10 +0200

    Initilize Empty Branch

--- src/chrome/actions.ts
+++
@@ -1,6 +0,0 @@
-export class Actions {
-  static start = "start";
-  static dsr1 = "dsr1";
-  static claude = "claude";
-  static setResponseIntoTextbox = "setResponseIntoTextbox";
-}
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  purge: ["./src/**/*.{ts,tsx,js,jsx,html}"],
-  darkMode: false,
-  theme: { extend: {} },
-  variants: { extend: {} },
-  plugins: [],
-  mode: "jit",
-};
